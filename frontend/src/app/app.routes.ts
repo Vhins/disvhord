06975c74e82aedb9372d2d468a36b4f027d2d4ed
Vhins_ <vincenzomanzi06@gmail.com>
2024-12-24 14:05:36 +0100

    Main site page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { provideRouter } from '@angular/router';
 
 import { MainSiteComponent } from './main-site/main-site.component';
 import { DisvhordAppComponent } from './disvhord-app/disvhord-app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 export const routes: Routes = [
     { path: '', component: MainSiteComponent },
     { path: 'app', component: DisvhordAppComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'login', loadComponent: () => import('./auth/login/login.component').then(c => c.LoginComponent) },
+    { path: 'register', loadComponent: () => import('./auth/register/register.component').then(c => c.RegisterComponent) },
     { path: '**', redirectTo: '' },
 ];
 
--- frontend/src/app/main-site/main-site.component.ts
+++ frontend/src/app/main-site/main-site.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-site',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main-site.component.html',
   styleUrl: './main-site.component.css'
 })
