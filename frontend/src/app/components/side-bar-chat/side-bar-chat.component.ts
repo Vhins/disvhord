2edd72ce4cc0602726b5093339f3ecf66627636c
Vhins_ <vincenzomanzi06@gmail.com>
2025-02-02 22:49:19 +0100

    ctrl z

--- frontend/src/app/components/side-bar-chat/side-bar-chat.component.ts
+++ frontend/src/app/components/side-bar-chat/side-bar-chat.component.ts
@@ -1,7 +1,6 @@
-import { Component, ViewChild, ViewChildren } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ChatService } from '../../chat.service';
 import { ChatComponent } from '../chat/chat.component';
-import { CallComponent } from '../call/call.component';
 
 @Component({
   selector: 'app-side-bar-chat',
@@ -14,7 +13,7 @@ export class SideBarChatComponent {
     user_chat_name!: string
     user_chat_logo!: string
 
-    constructor(private chatService: ChatService){}
+    constructor(private chatService: ChatService, public chatComponent: ChatComponent){}
 
     ngAfterContentChecked() {
         this.user_chat_name = this.chatService.users_info[this.chatService.chat_user_id]?.name
@@ -22,7 +21,7 @@ export class SideBarChatComponent {
     }
 
     callThisCha() {
-        console.log('cry')
+        console.log(this.chatComponent.callThisChat())
     }
     
 }
