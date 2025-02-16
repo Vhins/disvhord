a37158b91508a30fd964a3caa1607dc9f05b0fc6
Vhins_ <vincenzomanzi06@gmail.com>
2025-02-16 22:50:13 +0100

    _

--- frontend/src/app/components/chat/chat-input/chat-input.component.ts
+++ frontend/src/app/components/chat/chat-input/chat-input.component.ts
@@ -13,15 +13,15 @@ import { MessagesService } from '../messages.service';
 })
 export class ChatInputComponent implements OnInit {
     private input_zone = viewChild.required<ElementRef<HTMLSpanElement>>('input_zone')
-    private text_area = viewChild.required<ElementRef<HTMLTextAreaElement>>('text_area')
-    text_area_innerHTML: string = ''
+    message_lines: {type: 'text' | 'link', content: string}[][] = [[{type: 'text', content: ''}]]
+    current_line: number = 0
 
     constructor(public chatService: ChatService, private messagesService: MessagesService, private renderer: Renderer2) {}
 
     ngOnInit() {
         this.chatService.currentEditingMessageText$.subscribe( value => {
-            console.log('valuevaluevaluevalue', value)
-            this.createNewTextArea(value)
+            console.log('currentEditingMessageText value:', value)
+            this.message_lines = [[{type: 'text', content: `${value}`}]]
             if (value !== "") { 
                 setTimeout(() => this.adjustHeight())
             }
@@ -31,46 +31,26 @@ export class ChatInputComponent implements OnInit {
 
     onKeydown(event: KeyboardEvent) {
         console.log('KeyboardEvent', event)
-        if (event.key === 'Enter' && event.shiftKey) {
-            this.text_area_innerHTML = this.text_area_innerHTML + "\n"
-        }else if (event.key === 'Enter' && !event.shiftKey) {
-            event.preventDefault()
-            this.onSendMessage()
-        } else {
-            const allowedKeys = /^[a-zA-Z0-9\s.,?!]+$/
-            if (event.key === '(') {
-                this.text_area_innerHTML = this.text_area_innerHTML + event.key
-                console.log('text_area_innerHTML', this.text_area_innerHTML)
-                this.adjustHeight()
-            }
-            else if (event.key.length === 1 && allowedKeys.test(event.key)) {
-                this.text_area_innerHTML = this.text_area_innerHTML + event.key
-                console.log('text_area_innerHTML', this.text_area_innerHTML)
-                this.adjustHeight()
-            }
-        }
     }
 
     onSendMessage() {
-        if (this.text_area_innerHTML === "") return
-        const message = this.convertMessageToDatabaseFormat(this.text_area_innerHTML)        
+        if (this.message_lines[0][0].content === "") return
+        const message = this.convertMessageToDatabaseFormat(this.message_lines)        
 
         if (!this.chatService.editingMessageMode()) {
             this.messagesService.sendMessage(message)
-            this.text_area_innerHTML = ""
         } else {
             this.sendEditedMessage(message)
         }
 
-        ;(this.text_area().nativeElement as HTMLTextAreaElement).removeAttribute('style')
-        this.createNewTextArea()
+        this.resetInput()
         this.adjustHeight()
     }
 
     sendEditedMessage(inputValue: string) {
         if (this.chatService.currentIDMessageEditing === null) return
         this.messagesService.editMessage(this.chatService.currentIDMessageEditing, inputValue)
-        this.text_area_innerHTML = ""
+        this.resetInput()
         this.adjustHeight()
     }
 
@@ -80,53 +60,20 @@ export class ChatInputComponent implements OnInit {
     }
 
     onStopEditMessage() {
-        this.text_area_innerHTML = ""
+        this.resetInput()
         this.chatService.editingMessageMode.set(false)
         this.adjustHeight()
     }
 
-    last_textarea!: any
 
-    createNewTextArea(content?: string) {
-        console.log('createNewTextArea value:', content)
+    createNewDiv(type: string, content: string) {
 
-        let ppalle_span = this.input_zone().nativeElement as HTMLSpanElement
-
-        if (this.last_textarea) {
-            let bo = this.renderer.removeChild(ppalle_span, this.last_textarea)
-        }
-
-        const textarea = this.renderer.createElement('textarea');
-
-        // this.text_area().nativeElement.hidden = true
-
-        this.renderer.setAttribute(textarea, 'contenteditable', 'true');
-        this.renderer.setAttribute(textarea, 'placeholder', 'Scrivi un messaggio.......');
-        this.renderer.setAttribute(textarea, 'name', 'text');
-        this.renderer.setAttribute(textarea, 'rows', '14');
-        this.renderer.setAttribute(textarea, 'cols', '10');
-        this.renderer.setAttribute(textarea, 'wrap', 'soft');
-        this.renderer.setAttribute(textarea, 'maxlength', '1000');
-        this.renderer.addClass(textarea, 'input');
-
-        this.renderer.listen(textarea, 'keydown', (event: KeyboardEvent) => { 
-            console.log('event1:', event)
-            this.onKeydown(event)
-            console.log('event2:', event)
-        })
-
-        if (content) {
-            console.log('content', content)
-            // this.renderer.setProperty(textarea, 'value', content)
-            // this.renderer.setValue(textarea, content)
-            // this.renderer.setProperty(textarea, 'innerHTML', content);
-        }
-        textarea.innerHTML = this.text_area_innerHTML;
-
-        let rendered = this.renderer.appendChild(ppalle_span, textarea);
-        this.last_textarea = textarea
-        console.log('redereddd', rendered)
     }
+
+    resetInput() {
+        this.message_lines = [[{type: 'text', content: ''}]]
+    }
+
 
     onAddLinkPopup() {
         this.chatService.allegatingLink.set(true)
@@ -144,53 +91,24 @@ export class ChatInputComponent implements OnInit {
         }
     }
 
-    convertMessageToDatabaseFormat(content: string): string {
-        return content
-            .replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;')
-            .replace(/\r\n|\r|\n/g, '<br>')
-            .replace(/ /g, '&nbsp;')
+    convertMessageToDatabaseFormat(content: {type: 'text' | 'link', content: string}[][]): string {
+        // todo
+        return '_test_'
+        // return content
+            // .replace(/&/g, '&amp;')
+            // .replace(/</g, '&lt;')
+            // .replace(/>/g, '&gt;')
+            // .replace(/"/g, '&quot;')
+            // .replace(/'/g, '&#39;')
+            // .replace(/\r\n|\r|\n/g, '<br>')
+            // .replace(/ /g, '&nbsp;')
+            // // .replace(/https?:\/\/[^\s<>()\[\]{}]+(?=\s|[^\w-]|$)/g, (url) => `<a href="${url}" target="_blank">${url}</a>`)
             // .replace(/https?:\/\/[^\s<>()\[\]{}]+(?=\s|[^\w-]|$)/g, (url) => `<a href="${url}" target="_blank">${url}</a>`)
-            .replace(/https?:\/\/[^\s<>()\[\]{}]+(?=\s|[^\w-]|$)/g, (url) => `<a href="${url}" target="_blank">${url}</a>`)
-            .replace(/https?:\/\/[^\s<>()\[\]{}]*(?=(?!.*&nbsp;)[\s|[^\w-]|$])/g, (url) => `<a href="${url}" target="_blank">${url}</a>`)
-            .replace(/[\u200B-\u200D\uFEFF]/g, '')
+            // .replace(/https?:\/\/[^\s<>()\[\]{}]*(?=(?!.*&nbsp;)[\s|[^\w-]|$])/g, (url) => `<a href="${url}" target="_blank">${url}</a>`)
+            // .replace(/[\u200B-\u200D\uFEFF]/g, '')
     }
-
-
-    @ViewChild('input_box') input_box!: ElementRef
-    @ViewChild('input_container') input_container!: ElementRef
-
-    textarea_scrollHeight: string = "0px"
-    textarea_lineHeight: string = "0px"
-    textarea_height: string = "0px"
-    heightWidgetEdit_Attachment: string = "80"
-
-    // aggiungere un observer/signal per ascoltare il agiustare altezzza
+    
     adjustHeight(): void {
-        /*
-        ;(this.text_area().nativeElement as HTMLTextAreaElement).removeAttribute('style')
 
-        //! console.debug('adjustHeight:', Number(this.textarea_scrollHeight.replace("px", "")), " : _ : ", this.textarea_scrollHeight)
-        if (Number(this.textarea_scrollHeight.replace("px", "")) > 102) { 
-            this.textarea_lineHeight = "30px"
-            this.textarea_height = `${this.textarea_scrollHeight}px`
-        } else {
-            this.textarea_lineHeight = "60px"
-            this.textarea_height = `${this.textarea_scrollHeight}px`
-        }
-
-        const input_box = this.input_box.nativeElement as HTMLDivElement
-
-        this.heightWidgetEdit_Attachment = String(Number(window.getComputedStyle(input_box).height.slice(0, -2)) + 10) + "px"
-
-        const input_container = this.input_container.nativeElement as HTMLDivElement
-        input_container.style.minHeight = `${42 + this.textarea_scrollHeight}px`
-
-        // const element = this.scrollContainer.nativeElement
-        // element.scrollTop = element.scrollHeight
-        */
     }
 }
