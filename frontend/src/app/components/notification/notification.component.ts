ea23b2bc276ed0c8523932aa88db9c8836185d6f
Vhins_ <vincenzomanzi06@gmail.com>
2025-03-01 15:00:09 +0100

    notifications

--- frontend/src/app/components/notification/notification.component.ts
+++ frontend/src/app/components/notification/notification.component.ts
@@ -1,6 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { NotificationsService } from '../../notifications.service';
+import { NotificationsService, NotificationType } from '../../notifications.service';
+import { ChatService } from '../chat/chat.service';
+
+interface NotificationData {
+    type: NotificationType,
+    image: string
+    name?: string
+}
 
 @Component({
   selector: 'app-notification',
@@ -10,20 +16,27 @@ import { NotificationsService } from '../../notifications.service';
   styleUrl: './notification.component.css'
 })
 export class NotificationComponent {
-    notificationType: "call" | "message" | null = null
+    notificationData: NotificationData | null = null
     _timeoutId: any = null
 
     notificationsService: NotificationsService = inject(NotificationsService)
+    chatService: ChatService = inject(ChatService)
 
     constructor() {
-        this.notificationsService.showNotification$.subscribe( type => {
-            this.notificationType = type
+        this.notificationsService.showNotification$.subscribe( data => {
+            if (!data) return
+
+            this.notificationData = {
+                type: data.type,
+                image: this.chatService.users_info[data.sender].img,
+                name: this.chatService.users_info[data.sender]?.name
+            }
 
             if (this._timeoutId) { clearTimeout(this._timeoutId) }
 
             this._timeoutId = setTimeout(() => {
-                this.notificationType = null
-            }, 5000)
+                this.notificationData = null
+            }, 50000)
         })
     }
 }
--- frontend/src/app/notifications.service.ts
+++ frontend/src/app/notifications.service.ts
@@ -3,21 +3,27 @@ import { WebSocketService } from './web-socket.service';
 import { Messages } from './components/chat/chat.model';
 import { BehaviorSubject } from 'rxjs';
 
+export type NotificationType = "call" | "message" | "friend_req" | "server_invite"
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
-    readonly showNotification$ = new BehaviorSubject<"call" | "message" | null>(null)
+    readonly showNotification$ = new BehaviorSubject<{type: NotificationType, sender: number} | null>(null)
     readonly incomingCall$ = new BehaviorSubject<any | null>(null)
 
     private webSocketService = inject(WebSocketService)
+    user_id = Number(localStorage.getItem("user_id"))
 
     listenForNotifications() {
         this.webSocketService.on("personal_message_received").subscribe((data: Messages) => {
-            this.showNotification$.next("message")
+            if (data.sender !== this.user_id) {
+                this.showNotification$.next({type: "message", sender: data.sender})
+            }
         })
         this.webSocketService.on("personal_call_started").subscribe(data => {
             this.incomingCall$.next(data)
+            this.showNotification$.next({type: "call", sender: data.sender})
         })
     }
   
